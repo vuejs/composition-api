185a2cc61444c16cf79b4c642bf708306aca222d
liximomo <liximomo@gmail.com>
2019-06-27 11:35:15 +0800

    fix: multisource-watch callback is called repeatedly

--- test/functions/watch.spec.js
+++ test/functions/watch.spec.js
@@ -23,13 +23,13 @@ describe('Hooks watch', () => {
           a,
         };
       },
-    });
-    expect(spy).toHaveBeenCalled();
+      template: `<div>{{a}}</div>`,
+    }).$mount();
     expect(spy.mock.calls.length).toBe(1);
     expect(spy).toHaveBeenLastCalledWith(1, undefined);
     vm.a = 2;
     expect(spy.mock.calls.length).toBe(1);
-    waitForWatcherFlush(() => {
+    waitForUpdate(() => {
       expect(spy.mock.calls.length).toBe(2);
       expect(spy).toHaveBeenLastCalledWith(2, 1);
     }).then(done);
@@ -45,37 +45,37 @@ describe('Hooks watch', () => {
           a,
         };
       },
-    });
-    expect(spy).toHaveBeenCalled();
+      template: `<div>{{a}}</div>`,
+    }).$mount();
     expect(spy.mock.calls.length).toBe(1);
     expect(spy).toHaveBeenLastCalledWith(1, undefined);
     vm.a = 2;
     expect(spy.mock.calls.length).toBe(1);
-    waitForWatcherFlush(() => {
+    waitForUpdate(() => {
       expect(spy.mock.calls.length).toBe(2);
       expect(spy).toHaveBeenLastCalledWith(2, 1);
     }).then(done);
   });
 
-  it('basic usage(multiple sources)', done => {
+  it('basic usage(multiple sources, lazy=false, flush=none-sync)', done => {
     const vm = new Vue({
       setup() {
         const a = value(1);
         const b = value(1);
-        watch([a, b], spy);
+        watch([a, b], spy, { lazy: false, flush: 'post' });
 
         return {
           a,
           b,
         };
       },
-    });
-    expect(spy).toHaveBeenCalled();
+      template: `<div>{{a}} {{b}}</div>`,
+    }).$mount();
     expect(spy.mock.calls.length).toBe(1);
     expect(spy).toHaveBeenLastCalledWith([1, 1], [undefined, undefined]);
     vm.a = 2;
     expect(spy.mock.calls.length).toBe(1);
-    waitForWatcherFlush(() => {
+    waitForUpdate(() => {
       expect(spy.mock.calls.length).toBe(2);
       expect(spy).toHaveBeenLastCalledWith([2, 1], [1, undefined]);
       vm.a = 3;
@@ -88,21 +88,138 @@ describe('Hooks watch', () => {
       .then(done);
   });
 
+  it('basic usage(multiple sources, lazy=true, flush=none-sync)', done => {
+    const vm = new Vue({
+      setup() {
+        const a = value(1);
+        const b = value(1);
+        watch([a, b], spy, { lazy: true, flush: 'post' });
+
+        return {
+          a,
+          b,
+        };
+      },
+      template: `<div>{{a}} {{b}}</div>`,
+    }).$mount();
+    vm.a = 2;
+    expect(spy).not.toHaveBeenCalled();
+    waitForUpdate(() => {
+      expect(spy.mock.calls.length).toBe(1);
+      expect(spy).toHaveBeenLastCalledWith([2, 1], [1, undefined]);
+      vm.a = 3;
+      vm.b = 3;
+    })
+      .then(() => {
+        expect(spy.mock.calls.length).toBe(2);
+        expect(spy).toHaveBeenLastCalledWith([3, 3], [2, 1]);
+      })
+      .then(done);
+  });
+
+  it('basic usage(multiple sources, lazy=false, flush=sync)', done => {
+    const vm = new Vue({
+      setup() {
+        const a = value(1);
+        const b = value(1);
+        watch([a, b], spy, { lazy: false, flush: 'sync' });
+
+        return {
+          a,
+          b,
+        };
+      },
+    });
+    expect(spy.mock.calls.length).toBe(1);
+    expect(spy).toHaveBeenLastCalledWith([1, 1], [undefined, undefined]);
+    vm.a = 2;
+    expect(spy.mock.calls.length).toBe(1);
+    waitForUpdate(() => {
+      expect(spy.mock.calls.length).toBe(2);
+      expect(spy).toHaveBeenLastCalledWith([2, 1], [1, undefined]);
+      vm.a = 3;
+      vm.b = 3;
+    })
+      .then(() => {
+        expect(spy.mock.calls.length).toBe(3);
+        expect(spy).toHaveBeenLastCalledWith([3, 3], [2, 1]);
+      })
+      .then(done);
+  });
+
+  it('basic usage(multiple sources, lazy=true, flush=sync)', done => {
+    const vm = new Vue({
+      setup() {
+        const a = value(1);
+        const b = value(1);
+        watch([a, b], spy, { lazy: true, flush: 'sync' });
+
+        return {
+          a,
+          b,
+        };
+      },
+    });
+    vm.a = 2;
+    expect(spy).not.toHaveBeenCalled();
+    waitForUpdate(() => {
+      expect(spy.mock.calls.length).toBe(1);
+      expect(spy).toHaveBeenLastCalledWith([2, 1], [1, undefined]);
+      vm.a = 3;
+      vm.b = 3;
+    })
+      .then(() => {
+        expect(spy.mock.calls.length).toBe(2);
+        expect(spy).toHaveBeenLastCalledWith([3, 3], [2, 1]);
+      })
+      .then(done);
+  });
+
+  it('out of setup', done => {
+    const obj = state({ a: 1 });
+    watch(() => obj.a, spy);
+    expect(spy.mock.calls.length).toBe(1);
+    expect(spy).toHaveBeenLastCalledWith(1, undefined);
+    obj.a = 2;
+    waitForUpdate(() => {
+      expect(spy.mock.calls.length).toBe(2);
+      expect(spy).toHaveBeenCalledWith(2, 1);
+    }).then(done);
+  });
+
+  it('out of setup(multiple sources)', done => {
+    const obj1 = state({ a: 1 });
+    const obj2 = state({ a: 2 });
+    watch([() => obj1.a, () => obj2.a], spy);
+    expect(spy.mock.calls.length).toBe(1);
+    expect(spy).toHaveBeenLastCalledWith([1, 2], [undefined, undefined]);
+    obj1.a = 2;
+    obj2.a = 3;
+    waitForUpdate(() => {
+      expect(spy.mock.calls.length).toBe(2);
+      expect(spy).toHaveBeenLastCalledWith([2, 3], [1, 2]);
+    }).then(done);
+  });
+
   it('multiple cbs (after option merge)', done => {
     const spy1 = jest.fn();
-    const obj = state({ a: 1 });
+    const a = value(1);
     const Test = Vue.extend({
       setup() {
-        watch(() => obj.a, spy1);
+        watch(a, spy1);
       },
     });
     new Test({
       setup() {
-        watch(() => obj.a, spy);
+        watch(a, spy);
+        return {
+          a,
+        };
       },
-    });
-    obj.a = 2;
-    waitForWatcherFlush(() => {
+      template: `<div>{{a}}</div>`,
+    }).$mount();
+    a.value = 2;
+    waitForUpdate(() => {
       expect(spy1).toHaveBeenCalledWith(2, 1);
       expect(spy).toHaveBeenCalledWith(2, 1);
     }).then(done);
@@ -118,10 +235,11 @@ describe('Hooks watch', () => {
           a,
         };
       },
-    });
+      template: `<div>{{a}}</div>`,
+    }).$mount();
     expect(spy).not.toHaveBeenCalled();
     vm.a = 2;
-    waitForWatcherFlush(() => {
+    waitForUpdate(() => {
       expect(spy).toHaveBeenCalledWith(2, 1);
     }).then(done);
   });
@@ -136,12 +254,13 @@ describe('Hooks watch', () => {
           a,
         };
       },
-    });
+      template: `<div>{{a}}</div>`,
+    }).$mount();
     const oldA = vm.a;
     expect(spy).not.toHaveBeenCalled();
     vm.a.b = 2;
     expect(spy).not.toHaveBeenCalled();
-    waitForWatcherFlush(() => {
+    waitForUpdate(() => {
       expect(spy).toHaveBeenCalledWith(vm.a, vm.a);
       vm.a = { b: 3 };
     })
@@ -238,11 +357,14 @@ describe('Hooks watch', () => {
           数据: a,
         };
       },
-    });
+      render(h) {
+        return h('div', this['数据']);
+      },
+    }).$mount();
     expect(spy).not.toHaveBeenCalled();
     vm['数据'] = 2;
     expect(spy).not.toHaveBeenCalled();
-    waitForWatcherFlush(() => {
+    waitForUpdate(() => {
       expect(spy).toHaveBeenCalledWith(2, 1);
     }).then(done);
   });
