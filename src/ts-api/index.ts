93a674342a27cd1572984afd74fa692c91fdf382
chucc <chucc@wangsu.com>
2019-06-29 21:22:33 +0800

    fix: Fix createComponent props type

--- src/ts-api/index.ts
+++ src/ts-api/index.ts
@@ -1,19 +1,34 @@
 // import Vue from 'vue';
-import Vue, { ComponentOptions } from 'vue';
+import Vue, { ComponentOptions, PropOptions, PropType } from 'vue';
 import { SetupContext } from '../types/vue';
 
-export type PropType<T> = T;
+// export type PropType<T> = T;
 
 // type FullPropType<T> = T extends { required: boolean } ? T : T | undefined;
 type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
+type PropValidator<T> = PropOptions<T> | PropType<T>;
+type RecordPropsDefinition<T> = {
+  [K in keyof T]: PropValidator<T[K]>;
+};
+type ArrayPropsDefinition<T> = (keyof T)[];
+type PropsDefinition<T> = ArrayPropsDefinition<T> | RecordPropsDefinition<T>;
 type ComponentOptionsWithSetup<Props> = Omit<ComponentOptions<Vue>, 'props' | 'setup'> & {
-  props?: Props;
+  props?: PropsDefinition<Props>;
   setup?: (
     this: undefined,
-    props: { [K in keyof Props]: Props[K] },
+    props: Readonly<Props>,
     context: SetupContext
   ) => object | null | undefined | void;
 };
+
+// when props is an object
+export function createComponent<Props>(
+  compOpions: ComponentOptionsWithSetup<Props>
+): ComponentOptions<Vue>;
+// when props is an array
+export function createComponent<Props extends string = never>(
+  compOpions: ComponentOptionsWithSetup<Record<Props, any>>
+): ComponentOptions<Vue>;
 
 export function createComponent<Props>(
   compOpions: ComponentOptionsWithSetup<Props>
