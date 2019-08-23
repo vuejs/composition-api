2c5f488e8078a22156c73d2f177e9b0605b84021
liximomo <liximomo@gmail.com>
2019-08-23 11:12:16 +0800

    fix: fix require() of plugin

--- test/setupTest.js
+++ test/setupTest.js
@@ -1,6 +1,6 @@
 const Vue = require('vue/dist/vue.common');
 const FullVue = require('vue/dist/vue.runtime.common');
-const { plugin } = require('../src');
+const plugin = require('../src').default;
 
 FullVue.config.productionTip = false;
 FullVue.config.devtools = false;
