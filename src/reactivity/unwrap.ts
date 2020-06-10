19991e11726e0c18a381e6c6e753931fa73f1159
Carlos Rodrigues <carlos@hypermob.co.uk>
2020-06-10 12:02:56 +0100

    chore: if the object is not extensible stop unwrapping

--- src/reactivity/unwrap.ts
+++ src/reactivity/unwrap.ts
@@ -1,45 +1,51 @@
-import { isRef } from './ref';
-import { proxy, isFunction, isObject, isArray } from '../utils';
-import { isReactive } from './reactive';
+import { isRef } from './ref'
+import { proxy, isFunction, isObject, isArray } from '../utils'
+import { isReactive } from './reactive'
 
 export function unwrapRefProxy(value: any) {
   if (isFunction(value)) {
-    return value;
+    return value
   }
 
   if (isRef(value)) {
-    return value;
+    return value
   }
 
   if (isArray(value)) {
-    return value;
+    return value
   }
 
   if (isReactive(value)) {
-    return value;
+    return value
   }
 
   if (!isObject(value)) {
-    return value;
+    return value
   }
 
-  const obj: any = {};
+  if (!Object.isExtensible(value)) {
+    return value
+  }
+
+  const obj: any = {}
 
   // copy symbols over
-  Object.getOwnPropertySymbols(value).forEach((s) => (obj[s] = (value as any)[s]));
+  Object.getOwnPropertySymbols(value).forEach(
+    (s) => (obj[s] = (value as any)[s])
+  )
 
   for (const k of Object.keys(value)) {
-    const r = value[k];
+    const r = value[k]
     // if is a ref, create a proxy to retrieve the value,
     if (isRef(r)) {
-      const set = (v: any) => (r.value = v);
-      const get = () => r.value;
+      const set = (v: any) => (r.value = v)
+      const get = () => r.value
 
-      proxy(obj, k, { get, set });
+      proxy(obj, k, { get, set })
     } else {
-      obj[k] = unwrapRefProxy(r);
+      obj[k] = unwrapRefProxy(r)
     }
   }
 
-  return obj;
+  return obj
 }
