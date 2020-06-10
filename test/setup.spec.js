34c0fad7b3d6182365ee0fb55fcd9b232e95320c
Preston Alvarado <700740+coolhome@users.noreply.github.com>
2020-06-10 06:56:43 -0400

    Handle non-reactive nulls (#231) (#241)
    * Fixes #231
    
    * Impl feedback
    
    * Handle undefined as well
    
    * Add unit test for undefined values
    
    * Update setup.spec.js
    
    * Apply suggestions from code review
    
    * Update test/setup.spec.js
    
    Co-authored-by: Eduardo San Martin Morote <posva@users.noreply.github.com>
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- test/setup.spec.js
+++ test/setup.spec.js
@@ -20,6 +20,30 @@ describe('setup', () => {
     expect(vm.a).toBe(1);
   });
 
+  it('should work with non reactive null', () => {
+    const vm = new Vue({
+      setup() {
+        return {
+          a: null,
+        };
+      },
+    }).$mount();
+    expect(vm.a).toBe(null);
+  });
+
+  it('should work with non reactive undefined', () => {
+    const vm = new Vue({
+      setup() {
+        return {
+          a: undefined,
+          b: 'foobar',
+        };
+      },
+    }).$mount();
+    expect(vm.a).toBe(undefined);
+    expect(vm.b).toBe('foobar');
+  });
+
   it('should be overrided by data option of plain object', () => {
     const vm = new Vue({
       setup() {
