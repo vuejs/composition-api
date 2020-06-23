f54fa48dfe25640f0a188bcdb8871e3da9e4270b
Anthony Fu <anthonyfu117@hotmail.com>
2020-06-23 12:18:18 +0800

    chore: update

--- src/nextTick.ts
+++ src/nextTick.ts
@@ -1,13 +1,11 @@
 import Vue from 'vue'
-import { currentVM, currentVue } from './runtimeContext'
+import { currentVue } from './runtimeContext'
 
 type NextTick = Vue['$nextTick']
 
-export const nextTick: NextTick = function nextTick(...args: any) {
-  if (currentVM) {
-    return currentVM.$nextTick.apply(currentVM, args)
-  } else {
-    // @ts-ignore
-    return currentVue?.nextTick(...args)
-  }
+export const nextTick: NextTick = function nextTick(
+  this: ThisType<NextTick>,
+  ...args: Parameters<NextTick>
+) {
+  return currentVue?.nextTick.bind(this, args)
 } as any
