87167ef1aa79cd0bf39f79a88184a32c40947643
Sebastian Krüger <2pi_r2@gmx.de>
2021-02-11 11:57:35 +0100

    remove comments

--- test-dts/defineAsyncComponent.test-d.ts
+++ test-dts/defineAsyncComponent.test-d.ts
@@ -57,26 +57,12 @@ defineAsyncComponent(
 )
 
 const component = defineAsyncComponent({
-  // The factory function
   loader: asyncComponent1,
-  // A component to use while the async component is loading
   loadingComponent: defineComponent({}),
-  // A component to use if the load fails
   errorComponent: defineComponent({}),
-  // Delay before showing the loading component. Default: 200ms.
   delay: 200,
-  // The error component will be displayed if a timeout is
-  // provided and exceeded. Default: Infinity.
   timeout: 3000,
-  // Defining if component is suspensible. Default: true.
   suspensible: false,
-  /**
-   *
-   * @param {*} error Error message object
-   * @param {*} retry A function that indicating whether the async component should retry when the loader promise rejects
-   * @param {*} fail  End of failure
-   * @param {*} attempts Maximum allowed retries number
-   */
   onError(error, retry, fail, attempts) {
     expectType<() => void>(retry)
     expectType<() => void>(fail)
