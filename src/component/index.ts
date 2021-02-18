8409f48216f82497984041513c25452b8fe15cad
Sebastian Krüger <2pi_r2@gmx.de>
2021-02-18 04:29:54 +0100

    feat: add defineAsyncComponent API (#644)

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,4 +1,5 @@
 export { defineComponent } from './defineComponent'
+export { defineAsyncComponent } from './defineAsyncComponent'
 export { SetupFunction, SetupContext } from './componentOptions'
 export { ComponentInstance, ComponentRenderProxy } from './componentProxy'
 export { Data } from './common'
