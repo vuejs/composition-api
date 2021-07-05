c4dfc1035f1ce8763429f82a21733bf0f668897c
null仔 <308241863@qq.com>
2021-07-05 08:26:21 +0800

    test: add test cases for reactivity/set (#744)
    Co-authored-by: webfansplz <>

--- test/v3/reactivity/set.spec.ts
+++ test/v3/reactivity/set.spec.ts
@@ -1,6 +1,24 @@
-import { set, ref, watchEffect } from '../../../src'
+import { set, reactive, ref, watch, watchEffect } from '../../../src'
 
 describe('reactivity/set', () => {
+  it('should not trigger reactivity on native object member assignment', () => {
+    const obj = reactive<{ a?: number }>({})
+    const spy = jest.fn()
+    watch(obj, spy, { deep: true, flush: 'sync' })
+    obj.a = 1
+    expect(spy).not.toHaveBeenCalled()
+    expect(obj).toStrictEqual({ a: 1 })
+  })
+
+  it('should trigger reactivity when using set on reactive object', () => {
+    const obj = reactive<{ a?: number }>({})
+    const spy = jest.fn()
+    watch(obj, spy, { deep: true, flush: 'sync' })
+    set(obj, 'a', 1)
+    expect(spy).toBeCalledTimes(1)
+    expect(obj).toStrictEqual({ a: 1 })
+  })
+
   it('should trigger watchEffect when using set to change value of array length', () => {
     const arr = ref([1, 2, 3])
     const spy = jest.fn()
