3a4c78b912a64ea0ba3c22ce75261218822d1eda
webfansplz <>
2021-08-17 10:12:51 +0800

    fix: typos

--- test/v3/reactivity/effectScope.spec.ts
+++ test/v3/reactivity/effectScope.spec.ts
@@ -190,7 +190,7 @@ describe('reactivity/effect/scope', () => {
     expect(doubled).toBe(undefined)
   })
 
-  it('should fire onDispose hook', () => {
+  it('should fire onScopeDispose hook', () => {
     let dummy = 0
 
     const scope = new EffectScope()
@@ -209,7 +209,7 @@ describe('reactivity/effect/scope', () => {
     expect(dummy).toBe(7)
   })
 
-  it('should warn onDispose() is called when there is no active effect scope', () => {
+  it('should warn onScopeDispose() is called when there is no active effect scope', () => {
     const spy = jest.fn()
     const scope = new EffectScope()
     scope.run(() => {
@@ -221,7 +221,7 @@ describe('reactivity/effect/scope', () => {
     onScopeDispose(spy)
 
     expect(
-      '[Vue warn]: onDispose() is called when there is no active effect scope to be associated with.'
+      '[Vue warn]: onScopeDispose() is called when there is no active effect scope to be associated with.'
     ).toHaveBeenWarned()
 
     scope.stop()
