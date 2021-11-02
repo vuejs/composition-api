6f335c58f715ceac92162728139a92ed6afa8a6d
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2021-11-03 00:23:39 +0800

    chore: update to ts 4.4

--- src/utils/instance.ts
+++ src/utils/instance.ts
@@ -176,7 +176,10 @@ export function activateCurrentInstance(
   setCurrentInstance(instance)
   try {
     return fn(instance)
-  } catch (err) {
+  } catch (
+    // FIXME: remove any
+    err: any
+  ) {
     if (onError) {
       onError(err)
     } else {
