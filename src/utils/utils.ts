d06db5b5fb4bfcacc7cba05c49c1789f208e2ba8
webfansplz <308241863@qq.com>
2022-01-29 10:42:18 +0800

    chore(watch): add comments

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -113,6 +113,10 @@ export function logError(err: Error, vm: Vue, info: string) {
   }
 }
 
+/**
+ * Object.is polyfill
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/is
+ * */
 export function isSame(value1: any, value2: any): boolean {
   if (value1 === value2) {
     return value1 !== 0 || 1 / value1 === 1 / value2
