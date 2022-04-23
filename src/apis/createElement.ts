d8dbd73e4de66abec96df7a2dfd2f6623914095f
neiyichao03 <nieyichao03@kuaishou.com>
2022-04-23 16:25:51 +0800

    fix: createElement() has been called outside of render function. when handleSuccessMessage function used #804

--- src/apis/createElement.ts
+++ src/apis/createElement.ts
@@ -2,11 +2,16 @@ import type { CreateElement } from 'vue'
 import { getVueConstructor, getCurrentInstance } from '../runtimeContext'
 import { defineComponentInstance } from '../utils/helper'
 import { warn } from '../utils'
+import type { ComponentInternalInstance } from '..'
 
 let fallbackCreateElement: CreateElement
 
-export const createElement = function createElement(...args: any) {
-  const instance = getCurrentInstance()?.proxy
+export const createElement = function (
+  this: ComponentInternalInstance | undefined,
+  ...args: any
+) {
+  // #804
+  const instance = this?.proxy ?? getCurrentInstance()?.proxy
   if (!instance) {
     __DEV__ &&
       warn('`createElement()` has been called outside of render function.')
